23536e0f1306c4837d1761f3130eaf781950c4ca
NDOLIC <tblaguese1@gmail.com>
2018-12-21 16:28:42 +0200

    var ansrs

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $("#controls form").submit(function(event) {
+      var ans1 = $("input[name='Quest1']:checked").val();
+      var ans2 = $("input[name='Quest2']:checked").val();
+      var ans3 = $("input[name='Quest3']:checked").val();
+      var ans4 = $("input[name='Quest4']:checked").val();
+      var ans5 = $("input[name='Quest5']:checked").val();
+      var ans6 = $("input[name='Quest6']:checked").val();
+      var ans7 = $("input[name='Quest7']:checked").val();
+    });
+});
